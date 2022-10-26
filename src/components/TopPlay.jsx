ee9e2eda80ce3b73116d8ecacf730c7b33449117
Raijin1302 <raijinnguyen1302@gmail.com>
2022-10-26 23:47:15 +0700

    change subtitle

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -22,7 +22,14 @@ const TopChartCard = ({ song, index }) => {
           src={song?.images?.coverart}
           alt={song?.title}
         />
-        <p>{song?.title}</p>
+        <div className="flex-1 flex flex-col justify-center mx-3">
+          <Link to={`/songs/${song?.key}`}>
+            <p className="text-xl font-bold text-white">{song?.title}</p>
+          </Link>
+          <Link to={`/songs/${song?.artists[0].adamid}`}>
+            <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
+          </Link>
+        </div>
       </div>
     </div>
   );
@@ -64,7 +71,7 @@ const TopPlay = () => {
         </div>
         <div className="mt-4 flex flex-col gap-1">
           {topPlays?.map((song, index) => (
-            <TopChartCard song={song} index={index} />
+            <TopChartCard key={song?.key} song={song} index={index} />
           ))}
         </div>
       </div>
