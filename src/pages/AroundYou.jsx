26a28ce0372066df6f02065c537b85c2c8174b8a
Raijin1302 <raijinnguyen1302@gmail.com>
2022-11-03 23:17:36 +0700

    try using geo api to get locatioon

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Error, Loader, SongCard } from "../components";
+const AroundYou = () => {
+  const [country, setCountry] = useState("");
+  const [loading, setLoading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const location = import.meta.env.VITE_GEO_KEY;
+  console.log(country);
+  useEffect(() => {
+    axios
+      .get(
+        `https://geo.ipify.org/api/v2/country?apiKey=${
+          import.meta.env.VITE_GEO_API_KEY
+        }`
+      )
+      .then((res) => setCountry(res?.data?.location.country))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [country]);
 
-const CountryTracks = () => <div>CountryTracks</div>;
+  return <div></div>;
+};
 
-export default CountryTracks;
+export default AroundYou;
