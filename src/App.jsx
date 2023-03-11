8b80cd4096bcaa01a6b5df4682a92c54ddf37515
Raijin1302 <raijinnguyen1302@gmail.com>
2023-03-12 01:02:23 +0700

    :hammer: naive fix router for handleClick

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,14 @@ const App = () => {
   return (
     <div className="relative flex">
       <Sidebar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+      <div className="flex-1 flex flex-col  bg-gradient-to-br from-black to-[#121286]">
         <Searchbar />
 
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Discover />} />
+              <Route path="/discover" element={<Discover />} />
               <Route path="/top-artists" element={<TopArtists />} />
               <Route path="/top-charts" element={<TopCharts />} />
               <Route path="/around-you" element={<AroundYou />} />
@@ -33,7 +34,7 @@ const App = () => {
               <Route path="/search/:searchString" element={<Search />} />
             </Routes>
           </div>
-          <div className="xl:sticky relative top-0 h-fit">
+          <div className="xl:sticky relative top-0 h-fit ">
             <TopPlay />
           </div>
         </div>
